644803a0df91f29e0795b01f1dbdb6fdf4fbf5c5
princekumar2022 <pk279183@gmail.com>
2023-07-20 19:32:05 +0530

    20 th july

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('../src/routes/routes');
+const routes = require('./routes/routes');
 const bodyParser = require('body-parser');
 
 
